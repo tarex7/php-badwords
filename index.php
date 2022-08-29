fcffc31f215bd4deab31091afda8dd1a3c6f62e2
tarex7 <tarex7@gmail.com>
2022-08-29 15:45:06 +0200

    added censorship

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde adipisci libero iste id distinctio qui minus eius neque molestiae deleniti. Repellat aliquam quo ipsa sed dolorem assumenda ea, error consectetur.";
+$paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. ";
 $paragraph_length = strlen($paragraph);
+$word = $_GET['word'];
+$censored = str_replace( $word, "***", $paragraph );
+$censored_length = strlen($censored)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,8 +14,10 @@ $paragraph_length = strlen($paragraph);
     <title>PHP</title>
 </head>
 <body>
-<p><?php echo $paragraph ?></p>
-<p>Lunghezza: <?php echo $paragraph_length ?> caratteri.</p>
-
+<p><strong>Paragrafo: </strong><?= $paragraph ?></p>
+<p><strong>Lunghezza:</strong> <?= $paragraph_length ?> caratteri.</p>
+<p><strong>Parola da censurare:</strong>  <?= $word ?>.</p>
+<p><strong>Testo censurato:</strong><?= $censored ?></p>
+<p><strong>Lunghezza testo censurato:</strong><?= $censored_length ?></p>
 </body>
 </html>
